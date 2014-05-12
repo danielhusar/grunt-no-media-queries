c5888dd1012782e84e27a6bc3e72c7121cef87c8
Daniel Husar <dano.husar@gmail.com>
2014-05-12 16:20:45 +0100

    travis grunt

--- tasks/no_media_queries.js
+++ tasks/no_media_queries.js
@@ -5,7 +5,6 @@ var nmq = require('no-media-queries');
 module.exports = function (grunt) {
   grunt.registerMultiTask('no_media_queries', 'Export the css without mediaqueries matching maximum page width.', function () {
     var options = this.options();
-
     eachAsync(this.files, function (el, i, next) {
       options.source = el.src[0];
       grunt.file.write(el.dest, nmq(grunt.file.read(el.src[0]), options.opts, options.rewokOts));
